3e742110b40fbee4ae0712ce90a3fddc94464692
ITTO Creative <ittocreative@gmail.com>
2026-07-20 12:10:47 +0700

    add language new page

---
+++ src/app/(admin)/admin/languages/new/page.tsx
@@ -0,0 +1,9 @@
+export default function NewLanguagePage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">
+        Tambah Language
+      </h1>
+    </div>
+  );
+}
